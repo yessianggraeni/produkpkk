7fc9dd9475a704b68c1e38e68489940bd2b28eb6
yessianggraeni <114454241+yessianggraeni@users.noreply.github.com>
2022-09-27 14:41:27 +0700

    Mengupload file index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Produk PKK</title>
+</head>
+<body>
+    <div class "nav">
+    <div class="menu-kiri">
+        <h2>nama web</h2>
+    </div>
+    <div class="menu-kanan">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li>menu2</li>
+        </ul>
+    </div>
+    </div>
+        <h1>ANGGOTA KELOMPOK</h1>
+        <table border="1">
+            <tr>
+                <th>foto</th>
+                <th>biodata</tr>
+    </body>
+    <tr>
+        <td><img src="Yesi.jpg"
+        width="50"><br></td>
+        <td>Nama : Yessi Anggraeni<br> TTL : TSM,10-08-2005<br> Alamat : kp tanjung wangi<br>
+        No HP:081903153771</td>
+    </tr>
+    
+    <tr>
+        <td><img src="Siti.jpg"
+        width="50"><br></td>
+        <td>Nama : siti<br> TTL : TSM,20-07-2005<br> Alamat :kp.cibodas<br>
+        No HP:085846278698</td>
+    </tr>
