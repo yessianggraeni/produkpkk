f6cf1514930257df832776f5def067bfcbd3b010
yessianggraeni <114454241+yessianggraeni@users.noreply.github.com>
2022-09-27 14:40:31 +0700

    Mengupload file produk.php

---
+++ produk.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    <title>Projek PKK</title>
+</head>
+<body>
+        <div class "nav">
+    <div class="menu-kiri">
+        <h2>nama web</h2>
+    </div>
+    <div class="menu-kanan">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="index.php">PRODUK</a></li>
+            </ul>
+            
+            </ul>
+            </div>
+            </div>
+                <br>
+        <h1>produk</h1>
+        <table border="1">
+              <tr>
+    </body>
+    
+    <tr>
+        <td><img src="Sandwich.jpg" width="50"></td>
+        <br><p>nama produk : Fruits sandwich</p>
+        <p>harga :8.000</p>
+        <p>deskrifsi :  fruit sandwich merupakan sandwich buah khas Jepang yang berisi cream lembut dan potongan buah-buah segar. Resep dan cara membuat sandwich buah khas Jepang ini sangatlah mudah dan membutuhkan sedikit bahan </p>
+        <button>PESAN</button>
+        </br>
+    </td>
+    
+                </ul>
+            </div>
+            </div>
+                <br>
+               <h1>produk</h1>
+        <table border="1">
+              <tr>
+    </body>
+    
+    <tr>
+        <td><img src="Puding.jpg" width="50"></td>
+        <br><p>nama produk :Puding oreo</p>
+        <p>harga : 2.500</p>
+        <p>deskrifsi :Puding adalah sebuah hidangan penutup yang umumnya dibuat dari bahan-bahan yang direbus, dikukus, atau dipanggang </p>
+        <button>PESAN</button>
+    </td>
